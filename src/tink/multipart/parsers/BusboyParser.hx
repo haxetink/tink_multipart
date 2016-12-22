69e8b7ca549038a38dd9d54d7ee594e1b5d202ce
Kevin Leung <kevinresol@gmail.com>
2016-12-22 23:58:20 +0800

    Try catch busboy

--- src/tink/multipart/parsers/BusboyParser.hx
+++ src/tink/multipart/parsers/BusboyParser.hx
@@ -23,38 +23,42 @@ class BusboyParser implements Parser {
 		var result = new Accumulator<Chunk>();
 		switch header.byName('content-type') {
 			case Success(v):
-				var busboy = new Busboy({headers: {'content-type': v}}); // busboy is only interested in the content-type header
-				busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-					Source.ofNodeStream('File part: $filename', file).all() // HACK: needa comsume the file otherwise busboy wont fire the 'finish' event
-						.handle(function(o) switch o {
-							case Success(bytes):
-								result.yield(Data({
-									name: fieldname,
-									body: File({filename: filename, mimeType: mimetype, content: bytes}),
-									header: null // TODO: reconstruct it?
-								}));
-							case Failure(e):
-								result.yield(Fail(e));
-						});
-				});
-				busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-					result.yield(Data({
-						name: fieldname,
-						body: Field(val),
-						header: null // TODO: reconstruct it?
-					}));
-				});
-				busboy.on('finish', function() {
-					result.yield(End);
-				});
-				busboy.on('error', function(e) result.yield(Fail(Error.withData('Busboy errored', e))));
-				
-				source.pipeTo(Sink.ofNodeStream('Busboy', busboy)).handle(function(o) switch o {
-					case AllWritten: // ok
-					case SinkEnded: result.yield(Fail(new Error('Sink Ended')));
-					case SinkFailed(e): result.yield(Fail(e));
-				});
-				
+				try {
+					var busboy = new Busboy({headers: {'content-type': v}}); // busboy is only interested in the content-type header
+					busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+						Source.ofNodeStream('File part: $filename', file).all() // HACK: needa comsume the file otherwise busboy wont fire the 'finish' event
+							.handle(function(o) switch o {
+								case Success(bytes):
+									result.yield(Data({
+										name: fieldname,
+										body: File({filename: filename, mimeType: mimetype, content: bytes}),
+										header: null // TODO: reconstruct it?
+									}));
+								case Failure(e):
+									result.yield(Fail(e));
+							});
+					});
+					busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+						result.yield(Data({
+							name: fieldname,
+							body: Field(val),
+							header: null // TODO: reconstruct it?
+						}));
+					});
+					busboy.on('finish', function() {
+						result.yield(End);
+					});
+					busboy.on('error', function(e) result.yield(Fail(Error.withData('Busboy errored', e))));
+					
+					source.pipeTo(Sink.ofNodeStream('Busboy', busboy)).handle(function(o) switch o {
+						case AllWritten: // ok
+						case SinkEnded: result.yield(Fail(new Error('Sink Ended')));
+						case SinkFailed(e): result.yield(Fail(e));
+					});
+				} catch(e:Dynamic) {
+					// busboy's constructor may throw js.Error
+					result.yield(Fail(new Error(e.message, e.data)));
+				}
 				
 			case Failure(_):
 				result.yield(Fail(new Error('Content-Type header not present')));
@@ -65,5 +69,5 @@ class BusboyParser implements Parser {
 
 @:jsRequire('busboy')
 extern class Busboy extends js.node.stream.Writable<Busboy> {
-	function new(options:{headers:Dynamic<String>});
+	function new(options:{headers:Dynamic<String>}); // TODO: more options?
 }
