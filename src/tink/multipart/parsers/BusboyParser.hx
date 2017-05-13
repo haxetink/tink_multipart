f2fa361a7b55728663022a2aae19c0232ff06cba
Kevin Leung <kevinresol@gmail.com>
2017-05-13 18:22:38 +0800

    Update lib

--- src/tink/multipart/parsers/BusboyParser.hx
+++ src/tink/multipart/parsers/BusboyParser.hx
@@ -3,7 +3,7 @@ package tink.multipart.parsers;
 import tink.multipart.Parser;
 import tink.multipart.Chunk;
 import tink.streams.RealStream;
-import tink.streams.Accumulator;
+import tink.streams.Stream;
 import tink.http.StructuredBody;
 
 using tink.io.Sink;
@@ -19,34 +19,35 @@ class BusboyParser implements Parser {
 	}
 	
 	public function parse(source:IdealSource):RealStream<Chunk> {
-		var result = new Accumulator<Chunk, Error>();
+		var trigger = Signal.trigger();
+		var result = new SignalStream(trigger);
 		try {
 			var busboy = new Busboy({headers: {'content-type': contentType}}); // busboy is only interested in the content-type header
 			busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
 				Source.ofNodeStream('File part: $filename', file).all() // HACK: needa comsume the file otherwise busboy wont fire the 'finish' event
 					.handle(function(o) switch o {
 						case Success(bytes):
-							result.yield(Data(new Named(fieldname, File(UploadedFile.ofBlob(filename, mimetype, bytes)))));
+							trigger.trigger(Data(new Named(fieldname, File(UploadedFile.ofBlob(filename, mimetype, bytes)))));
 						case Failure(e):
-							result.yield(Fail(e));
+							trigger.trigger(Fail(e));
 					});
 			});
 			busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-				result.yield(Data(new Named(fieldname, Value(val))));
+				trigger.trigger(Data(new Named(fieldname, Value(val))));
 			});
 			busboy.on('finish', function() {
-				result.yield(End);
+				trigger.trigger(End);
 			});
-			busboy.on('error', function(e:js.Error) result.yield(Fail(Error.withData(e.message, e))));
+			busboy.on('error', function(e:js.Error) trigger.trigger(Fail(Error.withData(e.message, e))));
 			
 			source.pipeTo(Sink.ofNodeStream('Busboy', busboy)).handle(function(o) switch o {
 				case AllWritten: // ok
-				case SinkEnded(_): result.yield(Fail(new Error('Sink Ended')));
-				case SinkFailed(e, _): result.yield(Fail(e));
+				case SinkEnded(_): trigger.trigger(Fail(new Error('Sink Ended')));
+				case SinkFailed(e, _): trigger.trigger(Fail(e));
 			});
 		} catch(e:Dynamic) {
 			// busboy's constructor may throw js.Error
-			result.yield(Fail(new Error(500, e.name + ': ' + e.message)));
+			trigger.trigger(Fail(new Error(500, e.name + ': ' + e.message)));
 		}
 		return result;
 	}
