7f0a83eadc1480286efa53b329bf8cd6b065a1c9
Kevin Leung <kevinresol@gmail.com>
2017-09-24 21:00:47 +0800

    Bug fixed upstream

--- src/tink/multipart/parsers/BusboyParser.hx
+++ src/tink/multipart/parsers/BusboyParser.hx
@@ -40,8 +40,7 @@ class BusboyParser implements Parser {
 				trigger.trigger(Data(new Named(fieldname, Value(val))));
 			});
 			busboy.on('finish', function() {
-				// TODO: investigate why we need hires=true here, otherwise the binding won't trigger
-				filesInProgress.observe().nextTime({hires: true}, function(v) return v == 0)
+				filesInProgress.observe().nextTime(function(v) return v == 0)
 					.handle(trigger.trigger.bind(End));
 			});
 			busboy.on('error', function(e:js.Error) trigger.trigger(Fail(Error.withData(e.message, e))));
